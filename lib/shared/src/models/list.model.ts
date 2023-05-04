4ff92c11c88d4b415f9efde8dfa3f7f55eda61d5
Elias Mawa <elias@emawa.io>
2023-05-03 20:38:59 -0600

    update: shared types

--- lib/shared/src/models/list.model.ts
+++ lib/shared/src/models/list.model.ts
@@ -3,6 +3,7 @@
 //***********************************************
 
 import { BaseCoverImage } from "./shared.model";
+import { BaseUser } from "./user.model";
 
 export type BaseList = {
   id: string;
@@ -16,6 +17,34 @@ export type BaseListItem = BaseCoverImage & {
   title: string;
   content: string;
   content_preview: string;
+  date_created?: string;
+  date_updated?: string;
+};
+
+export type BaseMemberList = BaseCoverImage & {
+  id: string;
+  list_items: BaseMemberListItem[];
+  date_created?: string;
+};
+
+export type BaseMemberListItem = BaseCoverImage & {
+  id: string;
+  first_name: string;
+  last_name: string;
+  title: string;
+  info: string;
+  email: string;
+  social_instagram: string;
+  roles: string[];
+  user: BaseUser;
+  date_created?: string;
+  date_updated?: string;
+};
+
+export type BaseCommunityBranchListItem = BaseCoverImage & {
+  id: string;
+  heading: string;
+  abbreviation: string;
   date_created?: string;
   date_updated?: string;
 };
